4a4bdb6932ef3cc9e583346994f632e56f03f49b
emilino acebith gonzalez aquino <112222363+ACEBITH@users.noreply.github.com>
2023-05-16 16:10:07 -0600

    cambios

---
+++ MOSTRAR IMAGEN/App.js
@@ -0,0 +1,10 @@
+const uno=()=>{
+    document.querySelector("#img").src="02.jpg"
+}
+
+const dos=()=>{
+    document.querySelector("#img").src="03.jpg"
+}
+const tres=()=>{
+    document.querySelector("#img").src="04.jpg"
+}
